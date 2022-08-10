7e40674061183eb397ac6072af8f1298c0076367
bvenceslas <bvenceslas@gmail.com>
2022-08-10 15:07:14 +0200

    Create auth middlewares

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { login, userUpdate, signUp, organisationUpdate} = require('../controllers/user-controller');
+const { login, userUpdate, signUp, settingUpdate } = require('../controllers/user-controller');
 
 router.post('/signup', signUp);
 router.post('/login', login);
 router.put('/update-details', userUpdate);
-router.put('/update-organisation', organisationUpdate);
+router.put('/update-settings', settingUpdate);
 
 module.exports = router;
